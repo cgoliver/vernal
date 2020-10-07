ecf25c1e5c15d8a6740495ea7f2c20a5155591c8
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2020-10-07 16:31:10 -0400

    env update

--- build_motifs/main.py
+++ build_motifs/main.py
@@ -4,17 +4,22 @@
 
 import os
 import sys
+import time
 import argparse
+import pickle
+
+if __name__ == "__main__":
+    pass
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(script_dir, '..'))
 
 def get_args():
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-    sys.path.append(os.path.join(script_dir, '..'))
-
-
     parser = argparse.ArgumentParser()
     # Meta graph Args
     parser.add_argument("--meta_graph", "-mg", type=str,
-                        default="", help="Path to meta-graph pickle. If none,\
+                        default="",
+                        help="Path to meta-graph pickle. If none,\
                             build a new one")
     parser.add_argument("--rgcn", "-r", type=str,
                                         default="default_name",
@@ -33,7 +38,7 @@ def get_args():
                                               Gaussian Mixture Model,\
                                               K-Means or Self-Organizing Maps")
     parser.add_argument("-N", "--n_components", type=int,
-                                                default=200
+                                                default=200,
                                                 help="components in the\
                                                 clustering")
     parser.add_argument('--prune', default=False,
@@ -45,10 +50,10 @@ def get_args():
 
     # Motif build args
 
-    parser.add_argument("--do_build", "-b", type=bool,
-                                            default=False,
-                                            action="store_true",
-                                            help="If True, build motifs from\
+    parser.add_argument("--do_build", "-b",
+                                      default=False,
+                                      action="store_true",
+                                      help="If True, build motifs from\
                                                   input meta-graph.")
     parser.add_argument("--levels", "-l", type=int,
                                           default=6,
@@ -59,7 +64,8 @@ def get_args():
                                       help="If True, only connect via backbone.")
 
     # Retrieve args
-    parser.add_argument("--do_retrieve", "-rt", type=bool,
+
+    parser.add_argument("--do_retrieve", "-rt",
                                             default=False,
                                             action="store_true",
                                             help="If True, build motifs from\
@@ -67,8 +73,29 @@ def get_args():
 
     return parser.parse_known_args()
 
-def build_mgraph():
-    pass
+def build_mgraph(args):
+    from build_motifs.meta_graph import MGraphAll
+    start = time.perf_counter()
+    mgg = MGraphAll(
+                    run = args.rgcn,
+                    clust_algo=args.clust_algo,
+                    n_components=args.n_components,
+                    optimize=False,
+                    min_edge=args.min_edge,
+                    max_var=args.max_var,
+                    max_graph=None,
+                    graph_diar=args.graphs,
+                    nc_only=args.nc
+                    )
+    print(f"Built Meta Graph in {time.perf_counter() - start} s")
+
+    if args.prune:
+        print("pruning")
+        mgg.prune()
+
+    print(f"Dumping meta graph in {args.mgg_name}")
+    pickle.dump(mgg, open(args.name + '.p', 'wb'))
+    return mgg
 
 def build_motifs():
     pass
@@ -77,7 +104,8 @@ def retrieve():
     pass
 
 def main():
-    args = get_args()
+    args,_ = get_args()
+    pass
     if args.meta_graph:
         mgraph = pickle.load(args.meta_graph)
     else:
