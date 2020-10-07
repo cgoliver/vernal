ad3ffbe540ad121de65a5c801296b981e72487f4
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2020-10-07 15:41:19 -0400

    master main

--- build_motifs/build.py
+++ build_motifs/build.py
@@ -54,7 +54,13 @@ def merge_nodesets(self,
 
     return merged_nodeset
 
-def maga_next(self, viz=False):
+def maga_next(maga_graph,
+              maga_tree,
+              mgraph,
+              boring_clusters,
+              min_edge=100,
+              levels=6,
+              max_boring=3):
     """
         Merge all connections between motifs and singletons.
         This means iterating through meta edges,
@@ -66,87 +72,83 @@ def maga_next(self, viz=False):
         Once merged, the new motif is size |motif| + 1,
     """
 
-    todo_edges = list(self.maga_graph.edges())
-    done_edges = list()
+    for l in range(levels):
+        todo_edges = list(maga_graph.edges())
+        done_edges = list()
 
-    to_kill = list()
+        to_kill = list()
 
-    for (singleton, motif) in tqdm(todo_edges):
+        for (singleton, motif) in tqdm(todo_edges):
 
-        # merge phase
-        new_node = motif.combine(singleton)
-        new_nodeset = self.merge_nodesets(singleton, motif)
+            # merge phase
+            new_node = motif.combine(singleton)
+            new_nodeset = merge_nodesets(singleton, motif)
 
-        # don't repeat the same edge
-        edgeset = set([singleton, motif])
-        if edgeset in done_edges:
-            continue
-        done_edges.append(edgeset)
+            # don't repeat the same edge
+            edgeset = set([singleton, motif])
+            if edgeset in done_edges:
+                continue
+            done_edges.append(edgeset)
 
-        # draw motif for visual check..
-        if viz:
-            self.viz_maganode(new_node, new_nodeset)
+            # remove merged edge from maga graph
+            try:
+                maga_graph.remove_edge(singleton, motif)
+            except nx.exception.NetworkXError:
+                pass
+            try:
+                maga_graph.remove_edge(motif, singleton)
+            except nx.exception.NetworkXError:
+                pass
 
-        # remove merged edge from maga graph
-        try:
-            self.maga_graph.remove_edge(singleton, motif)
-        except nx.exception.NetworkXError:
+            # get number of boring clusters in motif
+            boring_count = 0
+            for clust in new_node:
+                if clust in boring_clusters:
+                    boring_count += 1
+            if boring_count > max_boring:
+                continue
+
+            # if new motif is too sparse, move to next one
+            # if not, add it to the meta graph
+            if len(new_nodeset) < min_edge:
+                continue
+            else:
+                maga_graph.add_node(new_node, node_set=new_nodeset)
+                maga_tree.add_edge(motif, new_node)
+
+            # connect phase
+            # for each cluster in the motif, add connection to other clutters
+            # e.g. for ABC, look at all singletons adjacent to A, B, and C
+            for clust in new_node:
+                for nei in mgraph.neighbors(clust):
+                    maga_graph.add_edge(ms.FrozenMultiset([nei]), new_node)
             pass
-        try:
-            self.maga_graph.remove_edge(motif, singleton)
-        except nx.exception.NetworkXError:
-            pass
 
-        # get number of boring clusters in motif
-        boring_count = 0
-        for clust in new_node:
-            if clust in self.boring_clusters:
-                boring_count += 1
-        if boring_count > 3:
-            continue
+            # if new motif is similar in size to the old one,
+            # kill the old motif
+            pre_merge_size = len(maga_graph.nodes[motif]['node_set'])
+            if len(new_nodeset) >= .8 * pre_merge_size:
+                to_kill.append(motif)
+                continue
 
-        # if new motif is too sparse, move to next one
-        # if not, add it to the meta graph
-        if len(new_nodeset) < self.min_edge:
-            continue
-        else:
-            self.maga_graph.add_node(new_node, node_set=new_nodeset)
-            self.maga_tree.add_edge(motif, new_node)
+        maga_graph.remove_nodes_from(to_kill)
 
-        # connect phase
-        # for each cluster in the motif, add connection to other clutters
-        # e.g. for ABC, look at all singletons adjacent to A, B, and C
-        for clust in new_node:
-            for nei in self.graph.neighbors(clust):
-                self.maga_graph.add_edge(ms.FrozenMultiset([nei]), new_node)
-        pass
-
-        # if new motif is similar in size to the old one,
-        # kill the old motif
-        pre_merge_size = len(self.maga_graph.nodes[motif]['node_set'])
-        if len(new_nodeset) >= .8 * pre_merge_size:
-            to_kill.append(motif)
-            continue
-
-    self.maga_graph.remove_nodes_from(to_kill)
-    pass
+        yield maga_graph
 
 
-def maga(self, levels=10):
-    maga_graph = nx.relabel_nodes(self.graph,
-                                    {n: ms.FrozenMultiset([n]) for n in self.graph})
+def maga(mgraph, levels=10):
+    maga_graph = nx.relabel_nodes(mgraph,
+                                    {n: ms.FrozenMultiset([n]) for n in mgraph})
 
     maga_graph = maga_graph.to_directed()
 
     # how many times to sample a cluster for boring ones
     n_boring_samples = 100
     # keep track of how many instances of each cluster are boring.
-    boring_clusters = {c: {'samples': 0.01, 'boring': 0} for c in set(self.labels)}
+    boring_clusters = {c: {'samples': 0.01, 'boring': 0} for c in set(mgraph.labels)}
 
     maga_tree = nx.DiGraph()
-    maga_tree.add_nodes_from((ms.FrozenMultiset([n]) for n in self.graph))
-
-    self.maga_tree = maga_tree
+    maga_tree.add_nodes_from((ms.FrozenMultiset([n]) for n in mgraph))
 
     # this dictionary is of the following form
     # {u: {c1: {v, w}, c2: {x}}}
@@ -155,7 +157,7 @@ def maga(self, levels=10):
     # in this case, node `u` is connected to clusters `c1` via `v`, `w`,
     # and connected to `c2` via `x`
 
-    self.maga_adj = defaultdict(def_set)
+    maga_adj = defaultdict(def_set)
 
     for n in maga_graph.nodes():
         maga_graph.nodes[n]['node_set'] = set()
@@ -163,36 +165,38 @@ def maga(self, levels=10):
         maga_graph[c1][c2]['edge_set'] = {frozenset([u, v]) for u, v, _ in d['edge_set']}
         for u, v in d['edge_set']:
 
-            self.maga_adj[u][self.labels[v]].add(v)
-            self.maga_adj[v][self.labels[u]].add(u)
+            maga_adj[u][mgraph.labels[v]].add(v)
+            maga_adj[v][mgraph.labels[u]].add(u)
 
             for node in (u,v):
-                clust = self.labels[node]
+                clust = mgraph.labels[node]
                 if boring_clusters[clust]['samples'] < n_boring_samples:
                     boring_clusters[clust]['samples'] += 1
-                    node_id = self.reversed_node_map[node]
+                    node_id = mgraph.reversed_node_map[node]
                     G = whole_graph_from_node(node_id)
                     if not has_NC_bfs(G, node_id, depth=1):
                         boring_clusters[clust]['boring'] += 1
 
-            u_node = ms.FrozenMultiset([self.labels[u]])
-            v_node = ms.FrozenMultiset([self.labels[v]])
+            u_node = ms.FrozenMultiset([mgraph.labels[u]])
+            v_node = ms.FrozenMultiset([mgraph.labels[v]])
             maga_graph.nodes[u_node]['node_set'].add(frozenset([u]))
             maga_graph.nodes[v_node]['node_set'].add(frozenset([v]))
 
     # consider a cluster boring if at at least 80% of instances are boring
-    self.boring_clusters = {clust for clust, counts in boring_clusters.items()\
+    boring_clusters = {clust for clust, counts in boring_clusters.items()\
                             if counts['boring'] / counts['samples'] > .8}
-    self.maga_graph = maga_graph
-    for l in range(levels):
+
+    maga_build = maga_next(maga_graph)
+    for l, maga_graph in enumerate(maga_build,
+                                   maga_tree,
+                                   maga_adj,
+                                   mgraph,
+                                   boring_clusters,
+                                   levels=levels):
         print("maga level ", l)
-        print("maga nodes ", len(self.maga_graph.nodes()),
-                "maga edges ", len(self.maga_graph.edges())
+        print("maga nodes ", len(maga_graph.nodes()),
+                "maga edges ", len(maga_graph.edges())
                 )
-        self.maga_next()
-
-    del self.maga_adj
-
 
 if __name__ == "__main__":
     pass
--- build_motifs/main.py
+++ build_motifs/main.py
@@ -6,9 +6,91 @@ import os
 import sys
 import argparse
 
-if __name__ == "__main__":
+def get_args():
     script_dir = os.path.dirname(os.path.realpath(__file__))
     sys.path.append(os.path.join(script_dir, '..'))
 
-if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    # Meta graph Args
+    parser.add_argument("--meta_graph", "-mg", type=str,
+                        default="", help="Path to meta-graph pickle. If none,\
+                            build a new one")
+    parser.add_argument("--rgcn", "-r", type=str,
+                                        default="default_name",
+                                        help="ID of trained embedding model")
+    parser.add_argument("--graphs", "-g", type=str,
+                                          default="data/rna_graphs_nr",
+                                          help="Path to full graphs.")
+    parser.add_argument('--name', type=str,
+                                  default="default_name",
+                                  help="The name of the pickled meta graph.")
+    parser.add_argument('--clust_algo', type=str,
+                                        default="k_means",
+                                        help="The clustering algo to use to \
+                                              build the meta_graph : possible \
+                                              choices are:\
+                                              Gaussian Mixture Model,\
+                                              K-Means or Self-Organizing Maps")
+    parser.add_argument("-N", "--n_components", type=int,
+                                                default=200
+                                                help="components in the\
+                                                clustering")
+    parser.add_argument('--prune', default=False,
+                                   action='store_true',
+                                   help="To make the meta graph sparser,\
+                                        remove infrequent edges")
+    parser.add_argument("--nc", default=False, action='store_true',
+                                help="To use only nc"),
+
+    # Motif build args
+
+    parser.add_argument("--do_build", "-b", type=bool,
+                                            default=False,
+                                            action="store_true",
+                                            help="If True, build motifs from\
+                                                  input meta-graph.")
+    parser.add_argument("--levels", "-l", type=int,
+                                          default=6,
+                                          help="Maximum motif size (number of \
+                                                merge operations)")
+    parser.add_argument('--backbone', "-bb", default=False,
+                                             action='store_true',
+                                      help="If True, only connect via backbone.")
+
+    # Retrieve args
+    parser.add_argument("--do_retrieve", "-rt", type=bool,
+                                            default=False,
+                                            action="store_true",
+                                            help="If True, build motifs from\
+                                                  input meta-graph.")
+
+    return parser.parse_known_args()
+
+def build_mgraph():
     pass
+
+def build_motifs():
+    pass
+
+def retrieve():
+    pass
+
+def main():
+    args = get_args()
+    if args.meta_graph:
+        mgraph = pickle.load(args.meta_graph)
+    else:
+        build_mgraph(args)
+
+    if args.do_build:
+        build_motifs(args)
+
+    if args.do_retrieve:
+        retrieve(args)
+    pass
+
+if __name__ == "__main__":
+    main()
+    pass
+
