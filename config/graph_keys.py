f3522c1dcade80cbeea922fd86072dad73dae0d8
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2021-06-09 20:24:56 -0400

    config folder

---
+++ config/graph_keys.py
@@ -0,0 +1,4 @@
+GRAPH_KEYS = {'nt_position': {'RGLIB': 'nt_resnum', 'FR3D': 'pdb_pos'},
+              'chain':       {'RGLIB': 'chain_name', 'FR3D': 'chain'},
+              'bp_type':     {'RGLIB': 'LW', 'FR3D': 'label'}
+              }
